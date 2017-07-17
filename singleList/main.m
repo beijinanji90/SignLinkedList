f78c15dc541ffbebfcb4ebbed74a4937735ba25b
chenfenglong <chenfenglong@koudai.com>
2017-07-17 22:48:31 +0800

    添加翻转单链表；倒序打印单链表

--- singleList/main.m
+++ singleList/main.m
@@ -35,6 +35,23 @@ LinkedList LinkedListInit()
 }
 
 /*
+ * 打印单链表
+ */
+void PrintSingleList(Node *pHead)
+{
+    if (pHead == NULL) {
+        return;
+    }
+    if (pHead->next) {
+        NSLog(@"%ld",(long)pHead->data);
+    }
+    while (pHead->next) {
+        NSLog(@"%ld",(long)pHead->data);
+        pHead = pHead->next;
+    }
+}
+
+/*
  * 初始化一个nodeNum个数的长度的链表
  */
 void CreateListNode(Node *pHead,int nodeNum)
@@ -51,12 +68,18 @@ void CreateListNode(Node *pHead,int nodeNum)
 }
 
 /*
- * 显示某一个节点的值
+ * 打印所有单链表的值
  */
-void ShowNodeData(Node *pHead)
+void ShowAllNodeData(Node *pHead)
 {
-    while (pHead != nil && pHead->next != NULL) {
-        printf("%d\n",pHead->data);
+    if (pHead == NULL) {
+        return;
+    }
+    if (pHead->next) {
+        NSLog(@"%ld",(long)pHead->data);
+    }
+    while (pHead->next) {
+        NSLog(@"%ld",(long)pHead->data);
         pHead = pHead->next;
     }
 }
@@ -64,7 +87,7 @@ void ShowNodeData(Node *pHead)
 /*
  * 查找链表的第m个元素
  */
-void findOneElement(Node *pHead,int m)
+void FindOneElement(Node *pHead,int m)
 {
     NSInteger i = 0;
     Node *originNode = pHead;
@@ -81,13 +104,49 @@ void findOneElement(Node *pHead,int m)
     }
 }
 
+/*
+ * 翻转单链表
+ */
+void ReversalSingleList(Node *pHead)
+{
+    if (pHead == NULL || pHead->next == NULL) {
+        return;
+    }
+    Node *originHeader = pHead;
+    Node *newHeader = NULL;
+    while (originHeader->next) {
+        Node *nextOriginNode = originHeader->next;
+        originHeader->next = newHeader;
+        newHeader = originHeader;
+        originHeader = nextOriginNode;
+    }
+    PrintSingleList(newHeader);
+}
+
+/*
+ * 倒序打印单链表；通过使用递归来倒序打印单链表。
+ */
+void InvertedSingleList(Node *pHead)
+{
+    if (pHead == NULL) {
+        return;
+    }
+    if (pHead->next) {
+        pHead = pHead->next;
+        InvertedSingleList(pHead);
+        NSLog(@"%ld",(long)pHead->data);
+    }
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         Node *link = LinkedListInit();
         CreateListNode(link, 10);
-        ShowNodeData(link);
-        findOneElement(link, 2);
+        InvertedSingleList(link);
+        //        ReversalSingleList(link);
+        //        ShowNodeData(link);
+        //        FindOneElement(link, 2);
     }
     return 0;
 }
